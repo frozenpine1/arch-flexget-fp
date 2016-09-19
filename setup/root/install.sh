eadb6c599d487aba12c86da8543d5d6eb0b2a0dd
frozenpine1 <frozenpine@gmail.com>
2016-09-19 16:31:05 -0700

    Update install.sh

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -4,7 +4,7 @@
 set -e
 
 # define pacman packages
-pacman_packages="python2 python2-pip rar"
+pacman_packages="python2 python2-pip unrar"
 
 # install pre-reqs
 pacman -Sy --needed $pacman_packages --noconfirm
