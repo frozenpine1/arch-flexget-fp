1a7449c1976228ff19c164403d9896128ccd3488
frozenpine1 <frozenpine@gmail.com>
2016-09-19 16:29:48 -0700

    Latest

---
+++ setup/root/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# exit script if return code != 0
+set -e
+
+# define pacman packages
+pacman_packages="python2 python2-pip rar"
+
+# install pre-reqs
+pacman -Sy --needed $pacman_packages --noconfirm
+pip2 install --upgrade setuptools
+pip2 install flexget
+pip2 install transmissionrpc
+pip2 install deluge
+pip2 install rarfile
+
+# call aor script (arch official repo)
+# source /root/aur.sh
+
+# cleanup
+yes|pacman -Scc
+rm -rf /usr/share/locale/*
+rm -rf /usr/share/man/*
+rm -rf /tmp/*
