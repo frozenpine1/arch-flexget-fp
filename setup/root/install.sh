6c314f7ca344635803c96c55b32e7de9d20733db
frozenpine1 <frozenpine@gmail.com>
2016-09-19 21:23:55 -0700

    Update install.sh

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -4,7 +4,7 @@
 set -e
 
 # define pacman packages
-pacman_packages="python2 python2-pip unrar deluge-common"
+pacman_packages="python2 python2-pip unrar deluge"
 
 # install pre-reqs
 pacman -Sy --needed $pacman_packages --noconfirm
