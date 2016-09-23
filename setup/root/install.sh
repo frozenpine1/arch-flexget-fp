0fafb4214b81f0d97856b81d6c90db27f5348f6b
frozenpine1 <frozenpine@gmail.com>
2016-09-23 16:28:44 -0700

    Update install.sh

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -9,11 +9,11 @@ pacman_packages="python2 python2-pip unrar deluge"
 # install pre-reqs
 pacman -Sy --needed $pacman_packages --noconfirm
 pip2 install --upgrade setuptools
-#pip2 install service_identity
-#pip2 install flexget
-#pip2 install transmissionrpc
-#pip2 install deluge
-#pip2 install rarfile
+pip2 install service_identity
+pip2 install flexget
+pip2 install transmissionrpc
+pip2 install deluge
+pip2 install rarfile
 
 # call aor script (arch official repo)
 # source /root/aur.sh
