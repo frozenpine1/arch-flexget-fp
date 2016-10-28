85c6228447c237ba8117eeaea415d7bb65595ddd
frozenpine1 <frozenpine@gmail.com>
2016-10-28 15:33:17 -0700

    Update flexget.sh

--- apps/nobody/flexget.sh
+++ apps/nobody/flexget.sh
@@ -2,4 +2,4 @@
 
 echo "$(date "+%d.%m.%Y %T") : Starting DAEMON"
 flexget -c /config/config.yml web passwd $WEB_PASSWD
-flexget -c /config/config.yml daemon start
+flexget -c /config/config.yml daemon start --config-autoreload
